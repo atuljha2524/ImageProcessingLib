0a444ec98c1ffed13b9775de30b8d350440724e0
Jha <Atul.Jha@philips.com>
2020-09-07 10:50:00 +0530

    Add project files.

---
+++ ImageProcessingLib/ImageProcessor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageProcessingLib
+{
+    public class ImageProcessor
+    {
+        public string Process(string imageContent)
+        {
+            return "Image Processed Using Alpha Alorithm version 1.0.0.0";
+        }
+    }
+}
---
+++ MsPaint/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MsPaint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ImageProcessingLib.ImageProcessor obj = new ImageProcessingLib.ImageProcessor();
+            //Console.WriteLine(obj.Process("testContent"));
+            try
+            {
+                Console.WriteLine("Enter Image Processor Version - Options 1.0.0.0 or 2.0.0.0 ");
+                string versionNumber = Console.ReadLine();
+
+                //String Interpollation
+                string dllPath = $@"C:\Users\320105541\source\repos\ImageProcessingLib\ImageProcessingLib\bin\Debug\{versionNumber}\ImageProcessingLib.dll";
+
+                //Load assembly Dynamically
+                System.Reflection.Assembly _dllRef = System.Reflection.Assembly.LoadFile(dllPath);
+
+                //search for class by name ImageProcessor
+                System.Type _classRef = _dllRef.GetType("ImageProcessingLib.ImageProcessor");
+
+                //Instantiate Class
+                Object obj = System.Activator.CreateInstance(_classRef);
+
+                System.Reflection.MethodInfo _methodRef = _classRef.GetMethod("Process");
+
+                //Invoke Dynamically
+                object result = _methodRef.Invoke(obj, new object[] {"test Image"});
+
+                Console.WriteLine(result.ToString());
+            }
+            catch (Exception ex) {
+                    Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
